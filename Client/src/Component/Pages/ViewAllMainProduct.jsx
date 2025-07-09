3779ee4c84ef4f73cdceedf8fc4b4390e27e8028
ammar3040 <ammar15102004@gmail.com>
2025-07-09 13:17:18 +0530

    Final changes in whatsapp logo and product card and logos

--- Client/src/Component/Pages/ViewAllMainProduct.jsx
+++ Client/src/Component/Pages/ViewAllMainProduct.jsx
@@ -69,7 +69,7 @@ Explore Handpicked Crystal & Agate Treasures
 
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
       {allProducts.slice(0, 8).map((product) => (
-        <Link to={`/Product/${product._id}`} key={product._id}>
+       
           <div className="w-full">
             <ProductCard
               productImg={product?.mainImage?.url || '/fallback.png'}
@@ -82,7 +82,7 @@ Explore Handpicked Crystal & Agate Treasures
               ModelNumber={product.modelNumber}
             />
           </div>
-        </Link>
+     
       ))}
     </div>
 
--- Client/src/Component/Pages/ViewAllProduct.jsx
+++ Client/src/Component/Pages/ViewAllProduct.jsx
@@ -69,7 +69,7 @@ Explore Handpicked Crystal & Agate Treasures
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
         {allProducts.map((product) => (
-             <Link to={`/Product/${product._id}`} key={product._id}>
+           
           <div key={product._id} className="w-full">
             <ProductCard
               productImg={product?.mainImage?.url || '/fallback.png'} // Prevent crash
@@ -82,7 +82,7 @@ Explore Handpicked Crystal & Agate Treasures
               ModelNumber={product.modelNumber}
             />
           </div>
-          </Link>
+     
         ))}
       </div>
     </div>
